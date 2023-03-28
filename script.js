5382bc5ab707eb62823ca7650268536ef2f557d1
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-03-28 18:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const reset = document.getElementById("reset_button");
+const change = document.getElementById("change_button");
+
+reset.addEventListener("click", resetGrid);
+change.addEventListener("click", changeColor);
+
+function resetGrid() {
+  for (let i = 1; i <= 9; i++) {
+    const gridItem = document.getElementById(`${i}`);
+    gridItem.style.backgroundColor = "transparent";
+   // console.log(gridItem);
+  }
+}
+
+function changeColor() {
+  const blockId = document.getElementById("block_id");
+  const colorId = document.getElementById("colour_id");
+
+  if (!blockId.value) {
+    alert("Please enter block id");
+  }
+  if (!colorId.value) {
+    alert("Please enter colour id");
+  }
+  if (blockId.value>=9) {
+    alert("Invalid block Id");
+  }
+  
+  resetGrid();
+  console.log(blockId.value)
+  const gridItem = document.getElementById(`${blockId.value}`);
+  gridItem.style.backgroundColor = colorId.value;
+
+  blockId.value = "";
+  colorId.value = "";
+}
